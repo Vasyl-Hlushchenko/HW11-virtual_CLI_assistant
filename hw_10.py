64f0e4d66a7f99ac9cbeabe49295f743137cc0a9
Vasyl Hlushchenko <Gluschenkov88@gmail.com>
2022-10-23 13:21:51 +0300

    add file

---
+++ hw_10.py
@@ -0,0 +1,110 @@
+from collections import UserDict
+from datetime import datetime
+
+
+class AddressBook(UserDict):
+    #Класс AddressBook, который наследуется от UserDict, и мы потом добавим логику поиска по записям в этот класс.
+    current_index = 0
+    N = 2
+    
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def iterator(self):
+        show_list = []
+        names = [name for name in self.data]
+        while len(self.data) >= AddressBook.current_index:
+            for name in names[AddressBook.current_index: min(len(self.data), AddressBook.current_index + AddressBook.N)]:
+                if [birthday.value for birthday in self.data[name].birthday] != []:
+                    show_list.append(f"{self.data[name].name.value}: phone{[phone.value for phone in self.data[name].phones]}, birthday {([birthday.value for birthday in self.data[name].birthday][0]).strftime('%d.%m.%Y')}")
+                else:
+                    show_list.append(f"{self.data[name].name.value}: phone{[phone.value for phone in self.data[name].phones]}")
+            yield show_list
+            AddressBook.current_index += AddressBook.N
+            show_list = []
+
+
+class Record:
+    #Класс Record, который отвечает за логику добавления/удаления/редактирования необязательных полей и хранения обязательного поля Name.
+    def __init__(self, name, phone = None, birthday = None):
+        self.name = Name(name)
+        if phone:
+            self.phones = [Phone(phone)]
+        else:
+            self.phones = []
+        if birthday:
+            self.birthday = [Birthday(birthday)]
+        else:
+            self.birthday = []
+
+    def add_birthday(self, birthday):
+        self.birthday.append(Birthday(birthday))
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def delete_phone(self, phone_for_delete):
+        for phone in self.phones:
+            if phone.value == phone_for_delete:
+                self.phones.remove(phone)
+
+    def change_phone(self, phone_for_change, new_phone):
+        for index, phone in enumerate(self.phones):
+            if phone.value == phone_for_change:
+                self.phones[index] = Phone(new_phone)
+
+    def days_to_birthday(self):
+        if self.birthday:
+            birthday =  [birthday.value for birthday in self.birthday][0]
+            if ((birthday).replace(year=(datetime.now()).year)) > datetime.now():
+                print (f"to birthday {(((birthday).replace(year=(datetime.now()).year)) - datetime.now()).days} days")
+            else:
+                print (f"to birthday {(((birthday).replace(year=(datetime.now()).year + 1)) - datetime.now()).days} days")
+
+
+class Field:
+    #Класс Field, который будет родительским для всех полей, в нем потом реализуем логику общую для всех полей.
+    def __init__(self, value):
+        self.value = value
+
+
+class Name(Field):
+    #Класс Name, обязательное поле с именем.
+    pass
+    
+
+class Phone(Field):
+    def __init__(self, value):
+        self.__value = None
+        self.value = value
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value:str):
+        if not all((value.startswith('+380'), value[1:].isdigit(), len(value) == 13)):
+            raise ValueError(print("Your phone should be like this: +380888888888"))
+        self.__value = value
+
+
+class Birthday(Field):
+    def __init__(self, value):
+        self.__value = None
+        self.value = value
+
+    def __str__(self) -> str:
+        return datetime.strftime(self.__value, '%d.%m.%Y')
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        try:
+            date_birthday = datetime.strptime(value, '%d.%m.%Y')
+            self.__value = date_birthday
+        except:
+            raise ValueError(print("Your birthday should be like this: 20.12.2000"))
---
+++ hw_9.py
@@ -0,0 +1,134 @@
+from hw_10 import *
+
+CONTACTS = AddressBook()
+
+
+def input_error(handler):
+    def wrapper(*args, **kwargs):
+        try:
+            handler(*args, **kwargs)
+        except (ValueError, IndexError, UnboundLocalError):
+            print("Error. Give me correct name, phone or birthday, please")
+        except KeyError:
+            print("Error. Enter user name, please")
+    return wrapper
+
+
+def hello_handler():
+    print("How can I help you?")
+
+
+def quit_handler():
+    print("Good bye!")
+    quit()
+
+
+@input_error
+def add_contact_handler(var):
+    name = var.split()[1]
+    phone = var.split()[2]
+    if name in CONTACTS:
+        record = CONTACTS.data[name]
+        record.add_phone(phone)
+    else:
+        record = Record(name, phone)
+        CONTACTS.add_record(record)
+    print(f"New contact was added")
+
+
+@input_error
+def find_contact_handler(var):
+    for name, record in CONTACTS.items():
+        if name == var.split()[1]:
+            print(f"{name}: {[phone.value for phone in record.phones]}")
+
+
+@input_error
+def delete_contact_handler(var):
+    name = var.split()[1]
+    phone_for_delete = var.split()[2]
+    record = CONTACTS.data[name]
+    record.delete_phone(phone_for_delete)
+    print("Contact's phone was deleted")
+
+
+@input_error
+def change_contact_handler(var):
+    name = var.split()[1]
+    phone_for_change = var.split()[2]
+    new_phone = var.split()[3]
+    if phone_for_change.isdigit() and new_phone.isdigit():
+        record = CONTACTS.data[name]
+        record.change_phone(phone_for_change, new_phone)
+        print("Contact was changed")
+
+
+@input_error
+def add_birthday_handler(var): #delete???
+    name = var.split()[2]
+    birthday = var.split()[3]
+    if name in CONTACTS:
+        record = CONTACTS.data[name]
+        record.add_birthday(birthday)
+        print(f"Contact's birthday was added")
+
+
+@input_error
+def days_to_birthday_handler(var): #delete???
+    name = var.split()[0]
+    if name in CONTACTS:
+        record = CONTACTS.data[name]
+        record.days_to_birthday()
+
+
+def show_contacts_handler():
+    for name, record in CONTACTS.items():
+        if [birthday.value for birthday in record.birthday] != []:
+            print(f"{name}: phone{[phone.value for phone in record.phones]}, birthday {([birthday.value for birthday in record.birthday][0]).strftime('%d.%m.%Y')}")
+        else:
+            print(f"{name}: phone{[phone.value for phone in record.phones]}")
+
+
+def iteration(): #delete???
+    for i in CONTACTS.iterator():
+        print('************************************')
+        print(i)
+        print('************************************')
+
+
+COMMANDS = {
+    "hello": hello_handler,
+    "show all": show_contacts_handler,
+    "exit": quit_handler,
+    "close": quit_handler,
+    "good bye": quit_handler,
+    "iter": iteration #delete???
+}
+
+
+def main():
+    while True:
+        var = (input("Enter command: ")).lower()
+        if var.startswith('add birthday'): #delete???
+            add_birthday_handler(var)
+        elif var.endswith("birthday"): #delete???
+            days_to_birthday_handler(var)
+        elif var.startswith('add'):
+            add_contact_handler(var)
+        elif var.startswith('change'):
+            change_contact_handler(var)
+        elif var.startswith('phone'):
+            find_contact_handler(var)
+        elif var.startswith('delete'):
+            delete_contact_handler(var)
+        elif var not in COMMANDS:
+            print("Wrong command!")
+            continue
+        else:
+            COMMANDS[var]()
+
+
+
+
+if __name__ == "__main__":
+    main()
